eb74deeb73aa54bd449256e274650797fb4adc23
daniel gonzalez <danielgonzalez@MacStudio-de-daniel.local>
2025-01-14 17:42:42 +0100

    =a

--- postcss.config.js
+++ postcss.config.js
@@ -30,6 +30,7 @@ const purgecss = require("@fullhuman/postcss-purgecss")({
 
 module.exports = {
   plugins: [
+    require('autoprefixer'),
     ...(process.env.HUGO_ENVIRONMENT === "production" ? [purgecss] : []),
   ],
 };
